f3fb3098fce56d0a4607f21c7995030a3e6ede3b
serendipity <2483364539@qq.com>
2022-07-10 16:15:19 +0800

    修改了跨域 把允许请求的ip从http://localhost:8080改为任何ip

--- middleware/CORSMiddleware.go
+++ middleware/CORSMiddleware.go
@@ -7,7 +7,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		ctx.Writer.Header().Set("Access-Control-Max-Age", "86400")   //缓存时间
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "*") //可以通过访问的方法
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "*")
